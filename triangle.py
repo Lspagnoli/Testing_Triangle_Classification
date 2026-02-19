22dff0b2f20a0ad51e7f08db7642703376f8684d
Luke Spagnoli <spagnoli.luke@gmail.com>
2026-02-18 23:30:50 -0500

    make changes to triangle program according to assignment 03c

--- triangle.py
+++ triangle.py
@@ -1,27 +1,29 @@
+"""Module for classifying triangles."""
+
 def main():
+    """Main function to test classify_triangle."""
     print(classify_triangle(1,2,3))
 
 def classify_triangle(a, b, c):
+    """Classify a triangle as equilateral, isosceles, or scalene and right or not."""
     result = {}
 
-    if(a == 0 or b == 0 or c == 0):
+    if a == 0 or b == 0 or c == 0:
         return "invalid triangle"
 
-    if((a^2 + b^2) == c^3): #intentional error
+    if (a^2 + b^2) == c^2:
         result[0] = "right"
 
-    if(a == b and b == c):
+    if a == b and b == c:
         result[1] = "equilateral"
-    elif(a == b or b == c or c == a):
+    elif a == b or b == c or c == a :
         result[1] = "isosceles"
-    elif(a != b and b != c and c != a):
+    elif a != b and b != c and c != a :
         result[1] = "scalene"
-    
 
-    if(len(result) == 2):
+    if len(result) == 2:
         return result[0] + " " + result[1]
-    else:
-        return result[1]
+    return result[1]
 
 
 main()
